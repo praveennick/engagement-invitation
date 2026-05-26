8ed3cd78eb90b43c0bc515aa4993aa09a50f80f1
unknown <praveennick1@gmail.com>
2026-05-26 22:33:55 +0530

    changed the whatsap number

--- src/components/WeddingInvitation.jsx
+++ src/components/WeddingInvitation.jsx
@@ -19,7 +19,7 @@ const GOOGLE_MAPS_EMBED_URL =
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4525.768375667284!2d81.804959!3d17.006156300000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a37a3bf6658436f%3A0x9f226cd13ee3e272!2sS%20V%20Function%20Hall!5e1!3m2!1sen!2sin!4v1779564738906!5m2!1sen!2sin";
 
 const WHATSAPP_RSVP_URL =
-  "https://wa.me/919052166382?text=Hi%20Priyanka%2C%20We%20are%20happy%20to%20join%20Priyanka%20%26%20Praveen's%20wedding%20celebration.%20Looking%20forward%20to%20being%20there%20and%20blessing%20the%20couple.";
+  "https://wa.me/918801721959?text=Hi%20Priyanka%2C%20We%20are%20happy%20to%20join%20Priyanka%20%26%20Praveen's%20wedding%20celebration.%20Looking%20forward%20to%20being%20there%20and%20blessing%20the%20couple.";
 
 const FAMILY_CONTACT_NUMBERS = [
   { display: "+91 99597 77911", href: "+919959777911" },
